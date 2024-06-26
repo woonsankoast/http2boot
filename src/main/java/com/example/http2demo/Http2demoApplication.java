0a1978a33e4a16d88c35ab5c9c3bc125866527ba
고운산 <woonsan@koast.net>
2024-06-26 09:34:05 +0900

    http/2 test

---
+++ src/main/java/com/example/http2demo/Http2demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.http2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Http2demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Http2demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/http2demo/Http2demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.http2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Http2demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
